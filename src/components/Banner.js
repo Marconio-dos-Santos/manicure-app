fd3b4999ff0324832c39c1ab1b7cd13169507c09
Marconio dos Santos <marconio.dev@gmail.com>
2022-10-19 23:12:50 -0300

    add text of the banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 const Banner = () => {
   return (
-    <main className='relative after:bg-rose-100 after:absolute after:inset-0 after:z-1 after:bottom-52'>
-      <div className="relative mx-auto max-w-4xl pt-52 pb-8 sm:px-6 lg:px-8">
+    <main className='flex justify-center relative text-center after:bg-rose-100 after:absolute after:inset-0 after:z-1 after:bottom-52'>
+      <p className='z-20 absolute block text-6xl py-10 font-serif text-zinc-800 h-auto text-center md:text-7xl'>Cuide Bem de <br/> Suas Mãos</p>
+      <div className="relative mx-auto max-w-4xl pt-40 pb-8 sm:px-6 lg:px-8">
         <img
           className="relative w-full object-cover sm:h-72 md:h-96 z-10"
           src="pure-julia.jpg"
--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -3,21 +3,21 @@ import React from 'react'
 const Jobs = () => {
   return (
     <section id="jobs" className='container flex sm:flex-col mx-auto'>
-        <div className="md:flex mx-auto w-3/4 justify-around">
+        <div className="md:flex mx-auto w-3/4 justify-around gap-7">
             <div className=" flex flex-col py-10 items-center space-x-4 text-center">
-                <img className='w-1/3 pb-4' src="https://cdn-icons-png.flaticon.com/512/3499/3499179.png" alt="" />
+                <img className='w-1/3 pb-4' src="../h2-icon.png" alt="" />
                 <h3>Job 1</h3>
                 <p className=''>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, commodi aut pariatur nobis velit distinctio repellendus corrupti 
                 </p>
             </div>
             <div className="flex flex-col py-10 items-center space-x-4 text-center">
-                <img className='w-1/3 pb-4' src="https://cdn-icons-png.flaticon.com/512/3499/3499179.png" alt="" />
+                <img className='w-1/3 pb-4' src="../h2-icon-1.png" alt="" />
                 <h3>Job 1</h3>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, commodi aut pariatur nobis velit distinctio repellendus corrupti 
                 </p>
             </div>
             <div className="flex flex-col py-10 items-center space-x-4 text-center">
-                <img className='w-1/3 pb-4' src="https://cdn-icons-png.flaticon.com/512/3499/3499179.png" alt="" />
+                <img className='w-1/3 pb-4' src="../h2-icon-2.png" alt="" />
                 <h3>Job 1</h3>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, commodi aut pariatur nobis velit distinctio repellendus corrupti 
                 </p>
