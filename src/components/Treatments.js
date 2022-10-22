2a16066422a234d893fa841493304eeb78f83106
Marconio dos Santos <marconio.dev@gmail.com>
2022-10-22 16:46:40 -0300

    responsividade component treatments

--- src/components/Treatments.js
+++ src/components/Treatments.js
@@ -2,14 +2,32 @@ import React from 'react'
 
 const Treatments = () => {
   return (
-    <section className='flex flex-col items-center sm:flex-row sm:justify-between relative text-left after:bg-gray-300 after:absolute after:inset-0 after:bottom-0 after:-top-10  after:sm:bg-gray-300 after:sm:absolute after:sm:inset-0 after:sm:-bottom-12 after:sm:-top-20 after:sm:left-44'>
-        <section className='sm:w-1/2 w-3/4 z-10 '>
-            <img className='' src="pricing.jpg" alt="" />
+    <section className='flex flex-col items-center pl-8 pr-4 md:flex-row md:justify-between relative text-start after:bg-gray-300 after:absolute after:inset-0 after:bottom-0 after:-top-10  after:md:bg-gray-300 after:md:absolute after:md:inset-0 after:md:-bottom-12 after:md:-top-20 after:md:left-44'>
+    <section className='flex flex-col justify-center w-full ml-[5%] md:ml-0 pr-[3%]'>
+      <section className='flex flex-col md:flex-row'>
+      <section className='z-10 w-full'>
+          <img className='' src="pricing.jpg" alt="" />
+      </section>
+      <section className='flex pt-10 w-full md:pl-5 '>
+        <section className='flex flex-col text-start z-10'>
+          <span>span</span>
+          <h2>Titulo</h2>
+          <div className='mb-10'>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </div>
+          <div>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </div>
         </section>
-        <section className='w-1/2 z-10 '>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias qui possimus sit quam esse blanditiis facilis consequatur ducimus tenetur dicta impedit reprehenderit dignissimos nihil recusandae, modi, enim magnam, hic expedita?</p>
-        </section>
+      </section>
+      </section>
     </section>
+</section>
+
   )
 }
 
